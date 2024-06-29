d6162027af888d48279f610c29114e45363c329b
leobewater <mmbee888@gmail.com>
2024-06-29 10:48:43 -0400

    add api view for creating user

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from .serializers import UserSerializer
+
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = User
+    permission_classes = [AllowAny]
