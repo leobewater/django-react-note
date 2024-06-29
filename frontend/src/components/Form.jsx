ac240bcb5ca805730f5a5888f76232899c0b3723
leobewater <mmbee888@gmail.com>
2024-06-29 14:17:14 -0400

    add loading indicator

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import api from '../api';
 import { useNavigate } from 'react-router-dom';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
+import LoadingIndicator from '../components/LoadingIndicator';
 import '../styles/Form.css';
 
 function Form({ route, method }) {
@@ -49,6 +50,7 @@ function Form({ route, method }) {
         onChange={(e) => setPassword(e.target.value)}
         placeholder='Password'
       />
+      {loading && <LoadingIndicator />}
       <button className='form-button' type='submit'>
         {name}
       </button>
