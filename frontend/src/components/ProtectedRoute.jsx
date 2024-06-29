5f662a36cecc4e69d5bea46585bed752e4d2a7a8
leobewater <mmbee888@gmail.com>
2024-06-29 12:35:16 -0400

    fix typo

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -34,7 +34,7 @@ function ProtectedRoute({ children }) {
   };
 
   const auth = async () => {
-    const token = localStorage.get(ACCESS_TOKEN);
+    const token = localStorage.getItem(ACCESS_TOKEN);
     if (!token) {
       setIsAuthorized(false);
       return;
